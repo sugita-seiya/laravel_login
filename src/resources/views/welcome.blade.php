f88520a97a4020d6ee09ffce0112ef9fb184d0e7
sugita-seiya <satreu16@ezweb.ne.jp>
2020-09-13 13:56:54 +0900

    top画面変更

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SEIYA
                 </div>
 
                 <div class="links">
